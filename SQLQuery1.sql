b270e41d0afaf831d47aafddf0ec5a207acc4fb2
Steven A. Jones <jones.steven.alexander@gmail.com>
2019-05-21 13:59:56 -0400

    Updated ordersFilled to orderFill.py and gave the code looping functionality for ease of use.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,6 +3,9 @@ use [laserInv]
 --DELETE FROM ordersFilledTEST;
 --DELETE FROM resupplyTEST;
 
+--SELECT * FROM ordersFilledTEST;
 --SELECT * FROM keyInventoryTEST JOIN ordersFilledTEST on keyInventoryTEST.keyNum = ordersFilledTEST.keyNum;
---SELECT * FROM keyInventoryTEST;
+SELECT * FROM keyInventoryTEST;
 --SELECT * FROM resupplyTEST;
+--UPDATE keyInventoryTEST SET invCount = '0';
+
