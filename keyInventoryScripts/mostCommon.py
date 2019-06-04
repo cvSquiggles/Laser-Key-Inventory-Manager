db4442b64abcf8252d0ccb363bbe2d22ce86d48e
Steven A. Jones <jones.steven.alexander@gmail.com>
2019-06-04 15:59:16 -0400

    Added Query and formatting functionality to mostCommon which displays keys in need of resupply at the moment.

--- keyInventoryScripts/mostCommon.py
+++ keyInventoryScripts/mostCommon.py
@@ -2,6 +2,7 @@
 
 import pyodbc
 import sys
+from tabulate import tabulate
 from utility import clear, divider, DBNAME
 
 modeQuery = '''
@@ -13,7 +14,7 @@ GROUP BY keyNum, keysUsed
 ),
 t2 AS
 (
-SELECT keyNum, max(countVal) blop
+SELECT keyNum, max(countVal) maxCountVal
 FROM t1
 GROUP BY keyNum
 ),
@@ -21,12 +22,46 @@ t3 AS
 (
 SELECT y.keyNum, x.keysUsed
 FROM t1 x, t2 y
-WHERE blop = countVal
-GROUP BY y.keyNum, x.keysUsed
-)
-SELECT keyNum, max(keysUsed) commonOrderSize
+WHERE countVal = maxCountVal AND x.keyNum = y.keyNum
+),
+t4 AS
+(
+SELECT keyNum, commonOrderSize
+FROM
+(
+select keyNum, max(keysUsed) commonOrderSize
 FROM t3
 GROUP BY keyNum
+) a
+GROUP BY keyNum, commonOrderSize
+),
+t5 AS
+(
+SELECT keyNum, avg(a.entry) as avgentryPerMonth 
+FROM 
+(
+select keyNum, month(submit_time) as month, count(1) as entry
+from ordersFilled 
+group by keyNum, month(submit_time)) a 
+GROUP BY keyNum
+),
+t6 AS
+(
+ SELECT x.keyNum, (x.commonOrderSize * y.avgEntryPerMonth) as keysNeeded
+ FROM t4 x, t5 y
+ WHERE y.keyNum = x.keyNum
+ GROUP BY x.keyNum, (x.commonOrderSize * y.avgEntryPerMonth)
+),
+t7 AS
+(
+SELECT *
+FROM keyInventory
+GROUP BY keyNum, invCount
+)
+SELECT x.keyNum, x.invCount, y.keysNeeded
+FROM t7 x, t6 y
+WHERE x.keyNum = y.keyNum AND x.invCount <= y.keysNeeded
+GROUP BY x.keyNum, x.invCount, y.keysNeeded;
 '''
 
 try:
@@ -39,27 +74,28 @@ try:
     c1 = db.cursor()
     c1.execute(modeQuery)
     results = c1.fetchall()
-    widths = []
-    columns = []
-    tavnit = '|'
-    separator = '+' 
-    max_col_length = max(list(map(lambda x: len(str(x[1])), results)))
+    print(tabulate(results, headers=['Key', 'Inventory Ct.', 'Avg. per month'], tablefmt='psql'))
+    # widths = []
+    # columns = []
+    # tavnit = '|'
+    # separator = '+' 
+    # max_col_length = max(list(map(lambda x: len(str(x[1])), results)))
 
-    for cd in c1.description:
-        widths.append(max(max_col_length, len(cd[0])))
-        columns.append(cd[0])
+    # for cd in c1.description:
+    #     widths.append(max(max_col_length, len(cd[0])))
+    #     columns.append(cd[0])
 
-    for w in widths:
-        tavnit += " %-"+"%ss |" % (w,)
-        separator += '-'*w + '--+'
+    # for w in widths:
+    #     tavnit += " %-"+"%ss |" % (w,)
+    #     separator += '-'*w + '--+'
 
-    print(separator)
-    print(tavnit % tuple(columns))
-    print(separator)
-    for row in results:
-        #print(tavnit, row)
-        print(("| {:<6} |"*len(row)).format(*row))
-    print(separator)
+    # print(separator)
+    # print(tavnit % tuple(columns))
+    # print(separator)
+    # for row in results:
+    #     #print(tavnit, row)
+    #     print(("| {:<6} |"*len(row)).format(*row))
+    # print(separator)
     input("Press enter to continue...")
     db.close()
     sys.exit()
